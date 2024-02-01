23019aaff81a46aebba79b64a211cd421880b6bf
Byshov92 <dimabyshov@gmail.com>
2024-02-01 18:15:02 +0200

    add files

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import userData from '../data/userData.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+
+import { Profile } from './Profile/profile';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
+export const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+};
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,18 @@
+import { FriendListItem } from '../FriendListItem/FriendListItem';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+      {friends.map(friend => {
+        return (
+          <FriendListItem
+            key={friend.id}
+            avatar={friend.avatar}
+            name={friend.name}
+            status={friend.isOnline}
+          />
+        );
+      })}
+    </ul>
+  );
+};
